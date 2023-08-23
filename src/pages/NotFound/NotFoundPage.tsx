59e618e31a0933798a94e66a25c8b0f85bd00170
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2023-08-23 08:46:17 +0300

    page not found

---
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import Style from "./notFounde.module.scss";
+
+export const NotFoundPage = () => {
+  return (
+    <div className={Style.container}>
+      {/* <FontAwesomeIcon icon={faFaceFrown} /> */}
+      <h2>404</h2>
+      <h3>Page not found</h3>
+    </div>
+  );
+};
