e3e5f91d6e29c77ae20c8426b6b8a89b3ed54ea5
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2023-08-24 14:31:03 +0300

    styles and 404
    feat: normalize css
    feat: page 404 with styles
    feat: mixins scss

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,9 +1,12 @@
+import { faFaceFrown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Style from "./notFounde.module.scss";
 
 export const NotFoundPage = () => {
   return (
     <div className={Style.container}>
-      {/* <FontAwesomeIcon icon={faFaceFrown} /> */}
+      <FontAwesomeIcon icon={faFaceFrown} />
       <h2>404</h2>
       <h3>Page not found</h3>
     </div>
