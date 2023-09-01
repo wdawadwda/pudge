01a5f4e378cf29346edc4f8747ca429c5bc7eede
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2023-09-01 15:59:31 +0300

    refactot

--- src/router/routerSchema.tsx
+++ src/router/routerSchema.tsx
@@ -4,9 +4,9 @@ import { MainLayout } from "~/layouts/MainLayout/MainLayout";
 import { ClientAgreement } from "~/pages/ClientAgreement/ClientAgreement";
 import { HomePage } from "~/pages/Home/HomePage";
 import { NotFoundPage } from "~/pages/NotFound/NotFoundPage";
+import { Rules } from "~/pages/Rules/Rules";
 
 import { links } from "./Links";
-import { Rules } from "~/pages/Rules/Rules";
 
 export const routerSchema = createBrowserRouter([
   {
