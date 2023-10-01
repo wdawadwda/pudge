de6b550abb1b38a99d3b6e92945967956df025d8
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2023-10-01 14:05:27 +0300

    scroll to top button
    feat: button

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,21 +1,26 @@
 import { Outlet } from "react-router-dom";
 
+import { useScrollToTopButton } from "~/entities/hooks/useScrollToTopButton";
 import { Footer } from "~/features/Layout/Footer/Footer";
 import { Navbar } from "~/features/Layout/Navbar/Navbar";
 import { NeonStrip } from "~/shared/ui/NeonStrip/NeonStrip";
+import { ScrollToTopButton } from "~/shared/ui/ScrollToTopButton/ScrollToTopButton";
 
 import layoutStyles from "./mainLayout.module.scss";
 
 export const MainLayout = () => {
+  const { isShowScrollButton, mainReference } = useScrollToTopButton();
+
   return (
     <div className={layoutStyles.container}>
       <Navbar />
       <NeonStrip color={"green"} />
-      <main>
+      <main ref={mainReference}>
         <Outlet />
       </main>
       <NeonStrip color={"green"} />
       <Footer />
+      {isShowScrollButton && <ScrollToTopButton />}
     </div>
   );
 };
